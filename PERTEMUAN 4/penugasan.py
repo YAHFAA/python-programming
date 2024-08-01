5b84625394109a8eef5942dc977812a38fbebbe2
YAHFAA <fabyansyahyahfa@gmail.com>
2024-08-01 12:13:30 +0700

    Add files via upload

---
+++ PERTEMUAN 4/penugasan.py
@@ -0,0 +1,25 @@
+x = 7
+
+#penjumlahan
+x +=5
+print("Penjumlahan: ", x )
+
+#pengurangan
+x -= 2
+print("Pengrangan: ", x)
+
+#perkalian
+x *=7
+print("perkalian: ", x)
+
+#pembagian
+x /=5
+print("pembagian: ", x)
+
+#modulus
+x %=4
+print("modulus: ", x)
+
+#perpangkatan
+x **=16
+print("pangkat: ", x)
