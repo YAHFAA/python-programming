837897bf18ef0870bd0a05766821a0922ca76d5a
YAHFAA <fabyansyahyahfa@gmail.com>
2024-08-27 09:44:52 +0700

    Add files via upload

---
+++ phyton.py
@@ -0,0 +1,18 @@
+tugas1 =float (input("masukan nilai tugas ke 1 anda"))
+tugas1 =float (input("masukan nilai tugas ke 1 anda"))
+tugas1 =float (input("masukan nilai tugas ke 1 anda"))
+
+b = 3 
+
+all = tugas1 = tugas2 = tugas3
+
+avv = all / b
+
+if avv >= 70:
+  print("kamu lulus")
+elif avv <50 and 69:
+  print("kamu harus perbaikan")
+elif avv >= 50:
+  print("kamu tidak lulus")
+else:
+  print("kamu tetap sekolah")
